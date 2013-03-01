ae082ef0e36698d6f841c821446a0e9d5d8ac469
Alexander Bubeck <mail@cybernotic.org>
2013-03-01 15:37:55 +0100

    stripping and some cleanup

--- code_analyse.py
+++ code_analyse.py
@@ -1,107 +1,15 @@
 #!/usr/bin/python
-import rospkg
-import roslib
 import sys
-import re
 import os
 import string
+from ros_parser import Parser
 
-CATEGORIES = ['sub', 'pub', 'srv', 'srv_called', 'param', 'param_set', 
-        'req_tf', 'prov_tf', 'goal', 'feedback', 'result', 'act_called']
-
-CAT_5C = ['comm', "conf", "coord", "comput", "compo"] # Red, Blue, Green, Grey, Yellow
-
-DEFAULT_ORDER = ['name', 'type', 'from', 'to', 'desc', 'default']
-
-GENERIC = '<([^>]*)>'   # <something>
-QSTRING = '"([^"]*)"'
-PSTRING = '["\']([^"\']*)["\']'   # not quite right, will break on "can't"
-NEXT_PARAM =  ',\s*([^\,]+)' # , something, 
-FINAL_PARAM = ',\s*([^\)]+)\)' # , something)
-
-
-PATTERNS = [
-#[CATEGORY,     PATTERN,                                            FIELD_LIST], 
-# Add empty patterns () for blank fields
-
-# C++
- ['name',       'ros::init\([^\)]*' + QSTRING + '\)',               ['name']],  
- ['spin',       'ros::spin\(\)',                                    []],
- ['publ',       'publish\(\s*',                                    []],  
- ['sub',        'subscribe' + GENERIC + '\(' + QSTRING,             ["type", "name"]], 
- ['sub',        'subscribe\(' + QSTRING + '\s*',                            ["name"]], 
- ['pub',        'advertise' + GENERIC + '\s*\(' + QSTRING,             ['type', 'name']],
- ['param',      'param()\(' + QSTRING + ', [^,]+, ([^\)]+)\)',      ['type', 'name', 'default']], 
- ['param',      'param' + GENERIC + '\(' + QSTRING + ', [^,]+' + FINAL_PARAM, ['type', 'name', 'default']],
- ['param',      'getParam\(' + QSTRING,                             ['name']], 
- ['param',      'param::get\(' + QSTRING,                           ['name']], 
- ['param_set',  'setParam\(' + QSTRING,                             ['name']], 
- ['param_set',  'param::set\(' + QSTRING,                           ['name']], 
- ['srv',        'advertiseService\(' + QSTRING,                     ['name']], #C++ service *SHOULD SET TYPE
- ['srv_called', 'advertiseService' + GENERIC + '\(' + QSTRING,      ['type', 'name']],
- ['nodehandle',       'ros::NodeHandle \s*',                        []],
- ['subscriber_def',       'ros::Subscriber \s*',                        []],
- ['publisher_def',       'ros::Publisher \s*',                        []],
-
-
-# Python
- ['name',       'rospy.init_node\(' + PSTRING,                      ['name']], 
- ['sub',        'rospy.Subscriber\('+ PSTRING + NEXT_PARAM + ',',   ['name', 'type']], 
- ['pub',        'rospy.Publisher\(' + PSTRING + FINAL_PARAM,        ['name', 'type']], 
- ['param',      'rospy.get_param\(' + PSTRING + '\)',               ['name']],  
- ['param',      'rospy.get_param\(' + PSTRING + FINAL_PARAM,        ['name', 'default']], 
- ['param_set',  'rospy.set_param\(' + PSTRING,                      ['name']], 
- ['srv',        'rospy.Service\(' + PSTRING + NEXT_PARAM + ',',     ['name', 'type']],
- ['srv_called', 'rospy.ServiceProxy\(' + PSTRING + FINAL_PARAM,     ['name', 'type']],
-
-# Python or C++
- ['req_tf',     'lookupTransform\(' + PSTRING + ', ' + PSTRING,     ['from', 'to']],
- ['prov_tf',    'sendTransform\(.*' + NEXT_PARAM + FINAL_PARAM,     ['from', 'to']], # DOES NOT WORK
-]
 
 
 
 class RosWiki:
     def __init__(self):
-        self.fields = {}
-
-    def parse_line_by_line(self, filename):
-        f = open(filename, 'r')
-        lines = f.readlines()
-        f.close()
-        mmap = {}
-        line_number = 0
-        parsed_lines = []
-        for line in lines:
-            found = 0
-            for (cat, pattern, fields) in PATTERNS:
-                for match in re.findall(pattern, line.strip()):
-                    print "###", line_number, match, fields, cat
-                    found = cat
-                    mmap = {}
-                    if len(fields) > 1:
-                        for key, value in zip(fields, match):
-                            mmap[key] = self.clean(value)
-                            #print "key, value: ", key, value
-                            #print "###########" 
-            parsed_lines.append([line_number, found, line.rstrip(), mmap])
-            line_number+=1
-        return parsed_lines
-
-    def parse(self, filename):
-        f = open(filename, 'r')
-        contents = f.read()
-
-        for (cat, pattern, fields) in PATTERNS:
-            for match in re.findall(pattern, contents):
-                mmap = {}
-                if len(fields) > 1:
-                    for key, value in zip(fields, match):
-                        mmap[key] = self.clean(value)
-                else:
-                    mmap[ fields[0] ] = match
-                self.add(cat, mmap)
-        
+        self.fields = {}        
 
     def add(self, category, mmap):
         if category in self.fields:
@@ -121,6 +29,7 @@ class RosWiki:
             return first.values()[0]
         return ''
 
+    # Creates XMI File based on parser output
     def model_print(self, code):
         f = open('model/generated.ros_package', 'w')
         #Create HEADER
@@ -147,26 +56,28 @@ class RosWiki:
         f.write( "</ros:Package>\n")
         f.close()
 
+    # Creates a colorized HTML page based on parser output
     def pretty_print(self, code):
         f = open('code_colored.html', 'w')
-        print "<html>"
-        print "<body>"
-        print """    <table summary=\"code examples\"> 
-        <tr>
-        <td><pre style="border: 1px solid #888;padding: 2px">"""
+        f.write( "<html>\n")
+        f.write( "<body>\n")
+        f.write( """    <table summary=\"code examples\">\n 
+        <tr>\n
+        <td><pre style="border: 1px solid #888;padding: 2px">\n""")
 
         for line in code:
             if(line[1] != 0):
                 if(line[1] == "param"):
-                    print "<font color=\"#00FF00\">" , line[2], "</font>" , line[3]
+                    f.write( "<font color=\"#00FF00\">" + str(line[2])+ "</font>" + str(line[3]) + "\n")
                 else:
-                    print "<font color=\"#FF0000\">" , line[2], "</font>" , line[3]
+                    f.write( "<font color=\"#FF0000\">" + str(line[2])+ "</font>" + str(line[3]) + "\n")
             else:
-                print line[2]
+                f.write( line[2] +"\n")
 
-        print "</pre>"
-        print "</body>"
-        print "</html>"
+        f.write( "</pre>\n" )
+        f.write( "</body>\n")
+        f.write( "</html>\n")
+        f.close()
 
     #Erstelle Grafisches Model des XMI
     def create_model_graph(self, code):
@@ -189,11 +100,9 @@ class RosWiki:
 
         
 if __name__ == '__main__':
-    wiki = RosWiki()
-    for arg in sys.argv[1:]:
-        wiki.pretty_print(wiki.parse_line_by_line(arg))
-
-    #print wiki.code()
-
+    if(len(sys.argv) == 2):
+        p = Parser(sys.argv[1])
+    w = RosWiki()
+    w.pretty_print(p.parsed_lines)
 
 
--- ros_parser.py
+++ ros_parser.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import rospkg
 import roslib
 import sys
@@ -85,6 +87,9 @@ class Parser():
 	"""A source code parser for ROS code based on ros_wiki node
 	Will analyze a ROS package and extract all information
 	"""
+
+	parsed_lines = []
+
 	def __init__(self, package_name):
 		self.packdat = PackageData()
 		r = rospkg.RosPack()
@@ -93,21 +98,57 @@ class Parser():
 		self.packdat.description = m.description
 		self.packdat.url = m.url
 		path = r.get_path(package_name)
+		cppfiles = []
 		for r,d,f in os.walk(path):
 			for files in f:
 				if files.endswith(".cpp"):
-					print self.parse_line_by_line(os.path.join(r,files))
+					cppfiles.append(os.path.join(r,files))
+					#print self.parse_line_by_line()
 					print os.path.join(r,files)
+		self.parsed_lines = self.parse_line_by_line(cppfiles)
 		#fill node information
 
+	# Deletes all comments, empty lines and parenthesis lines from source code
+	def strip_comments_and_parenteses(self, lines):
+		#REG EX for comments
+		comment_pattern = r"(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|(//.*)"
+		comment_pattern2 = r"(\s*)(.*?)(//.*)"
+		stripped_lines = []
+		stripped_lines2 = []
+		print "Before stripping: ", str(len(lines))
+		for line in lines:
+			found = 0
+			for match in re.findall(comment_pattern, line.strip()):
+				found = 1
+			if(found == 0 and len(line.strip()) != 0):
+				stripped_lines.append(line)
 
-	def parse_line_by_line(self, filename):
-		f = open(filename, 'r')
-		lines = f.readlines()
-		f.close()
+		print "After first stripping: ", str(len(stripped_lines))
+		#REG EX for parenthesis
+		parenthesis_pattern = r"^(\{|\})"
+		for line in stripped_lines:
+			found = 0
+			for match in re.findall(parenthesis_pattern, line.strip()):
+				found = 1
+			if(found == 0):
+				stripped_lines2.append(line)
+		print "After second stripping: ", str(len(stripped_lines2))
+
+		return stripped_lines2
+
+	# Parses lines for ROS source code
+	def parse_line_by_line(self, filenames):
+		lines = []
+		for filename in filenames:
+			f = open(filename, 'r')
+			lines = lines + f.readlines()
+			f.close()
 		mmap = {}
 		line_number = 0
 		parsed_lines = []
+
+		lines = self.strip_comments_and_parenteses(lines)
+
 		for line in lines:
 			found = 0
 			for (cat, pattern, fields) in PATTERNS:
@@ -122,7 +163,9 @@ class Parser():
 							#print "###########" 
 			parsed_lines.append([line_number, found, line.rstrip(), mmap])
 			line_number+=1
+		print "Hurray! Parsed " + str(len(lines)) + " lines."
 		return parsed_lines
 
 if __name__ == "__main__":
-	p = Parser("cob_hokuyo")
+	if(len(sys.argv) == 2):
+		p = Parser(sys.argv[1])
